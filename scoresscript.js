2b2405775a6d8b983f5768af4303bf569e4356e1
Myka Kugaya <mykakug129@gmail.com>
2020-04-25 01:07:26 -0700

    started scores js

---
+++ scoresscript.js
@@ -0,0 +1,31 @@
+var scoresList = document.querySelector("#scoreslist");
+
+//Function to create and show highscore list
+//how to make them ordered?
+function showList() {
+    var scores = localStorage.getItem("scores");
+    scores = JSON.parse(scores);
+    if (scores!==null) {
+        var item = scores[i];
+        for (i=0; i<scores.length; i++) {
+            var li = document.createElement("li");
+            li.textContent = item;
+            li.setAttribute("data-index", i);
+            scoresList.appendChild(li);
+        }  
+    }
+    else {
+        return;
+    }
+}
+
+//Function to clear highscore list
+var clearBtn = document.querySelector("#clearbutton");
+
+clearBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    var blank = document.querySelector("#blank");
+    blank.removeChild();
+    blank.createElement("ol");
+    blank.child[0].setAttribute("id", "scoreslist");
+})
