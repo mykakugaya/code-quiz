ca363de51491339ce900d66821db20f1957079cf
Myka Kugaya <mykakug129@gmail.com>
2020-04-25 18:24:09 -0700

    highscores list showing, still won't clear

--- scoresscript.js
+++ scoresscript.js
@@ -1,36 +1,37 @@
 var scoresList = document.querySelector("#scoreslist");
+var blank = document.querySelector("#blank");
 
 //Function to create and show highscore list
-//how to make them ordered?
 function showList() {
+    //sorts scores from highest to lowest
     var scores = localStorage.getItem("scores");
     scores = JSON.parse(scores).sort((a,b)=> Number(a.split(' - ')[0]) <  Number(b.split(' - ')[0]) ? 1 :
-    Number(a.split(' - ')[0]) >  Number(b.split(' - ')[0]) ? -1 : 0)
-    console.log(scores)
-    // if (scores!==null) {
-    //     var item = scores[i];
-    //     for (i=0; i<scores.length; i++) {
-    //         var li = document.createElement("li");
-    //         li.textContent = item;
-    //         li.setAttribute("data-index", i);
-    //         scoresList.appendChild(li);
-    //     }  
-    // }
-    // else {
-    //     return;
-    // }
-    //scores.map, scores.forEach  for(let i=0) for(let score of scores){}
+    Number(a.split(' - ')[0]) >  Number(b.split(' - ')[0]) ? -1 : 0);
+
+    //creates ordered list of initials with corresponding scores
+    if (scores.length==0) {
+        return;
+    }
+    else{
+        for (i=0; i<scores.length; i++) {
+            var li = document.createElement("li");
+            li.textContent = (i+1) + ". " + scores[i].split(' - ')[1] + " : " + scores[i].split(' - ')[0];
+            li.setAttribute("data-index", i);
+            scoresList.appendChild(li);
+        }
+    }
 }
 
-showList()
+showList();
 
 //Function to clear highscore list
 var clearBtn = document.querySelector("#clearbutton");
 
-clearBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    var blank = document.querySelector("#blank");
-    blank.removeChild();
-    blank.createElement("ol");
-    blank.child[0].setAttribute("id", "scoreslist");
+clearBtn.addEventListener("click", function() {
+    if (scores.length!==0) {
+        for (i=0; i<scores.length; i++) {
+            localStorage.clear("scores");
+            showList();
+        }
+    }
 })
--- script.js
+++ script.js
@@ -2,112 +2,120 @@ var timer = document.querySelector("#timer");
 var mainEl = document.querySelector("#main");
 var startBtn = document.querySelector("#button");
 var userScore = 0;
+//Questions with choices and correct answer index
 var q1 = {
     "question":"Commonly used data types DO NOT include:",
-    "choices":["strings", "booleans", "alerts", "numbers"],
-    "answer": 2
+    "choices":["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    "answer":2
 }
 var q2 = {
     "question":"The condition in an if/else statement is enclosed within ________.",
-    "choices":["quotes", "curly brackets", "parentheses", "square brackets"],
-    "answer": 2
+    "choices":["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+    "answer":2
 }
 var q3 = {
     "question":"Arrays in JavaScript can be used to store ________.",
-    "choices":["numbers and strings", "other arrays", "booleans", "all of the above"],
+    "choices":["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
     "answer":3
 }
 var q4 = {
     "question":"String values must be enclosed within ________ when being assigned to variables.",
-    "choices":["commas", "curly brackets", "quotes", "parentheses"],
+    "choices":["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
     "answer":2
 }
 var q5 = {
     "question":"A very useful tool used during development and debugging for printing content to the debugger is:",
-    "choices":["JavaScript", "terminal/bash", "for loops", "console log"],
+    "choices":["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"],
     "answer":3
 }
-
+//Questions array
 var questions = [q1, q2, q3, q4, q5];
-//saved scores in form of initials:score
+//Saved scores array in form of score-initials
 var scores = localStorage.getItem('scores') ? JSON.parse(localStorage.getItem('scores')) : []
+
 var current = 0;
 var secondsLeft = 60;
 var timerInterval;
 
-
 //Function to end quiz and submit score
 function endQuiz() {
-    var quiz = document.getElementById('quiz')
-    quiz.innerHTML = "<h2>All done!</h2>"
+    //Stop timer
+    secondsLeft = 0;
+    clearInterval(timerInterval);
+    
+    //Show final score
+    var quiz = document.getElementById('quiz');
+    quiz.innerHTML = "<h4>All done!</h4>";
     var p = document.createElement("p");
-    p.textContent = "Your final score is " + userScore;
-    quiz.appendChild(p)
-     clearInterval(timerInterval)
-     quiz.appendChild(document.createElement('input'));
-     var btn = document.createElement('button');
-     btn.textContent = "SUBMIT";
-     quiz.appendChild(btn);
-     btn.addEventListener('click', function(){
-         var initials = this.previousElementSibling.value;
-         scores.push(`${userScore} - ${initials}`)
-         localStorage.setItem("scores", JSON.stringify(scores));
+    p.textContent = "Your final score is " + userScore + ".";
+    quiz.appendChild(p);
+    
+    //Initials input
+    var p2 = document.createElement("p");
+    p2.textContent = "Enter initials: ";
+    quiz.appendChild(p2);
+    quiz.appendChild(document.createElement('input'));
+    var btn = document.createElement('button');
+    btn.textContent = "Submit";
+    quiz.appendChild(btn);
+
+    //Push items to scores array
+    btn.addEventListener('click', function() {
+        var initials = this.previousElementSibling.value;
+        scores.push(`${userScore} - ${initials}`);
+        localStorage.setItem("scores", JSON.stringify(scores));
+        window.location.href="highscores.html";
      })
-   
 }
-
-
 
 //Function to show next question
 function show() {
-    //when were on the last index
+    //when past the last index
     if(current === questions.length){
-        endQuiz()
-    }else{
-        //remove content
-    document.getElementById('content').style = "display:none"
-
-    //create new content
-    // var content = document.createElement("div");
-    // content.setAttribute("id", "content");
-    // mainEl.appendChild(content);
-    var quiz = document.getElementById('quiz');
-    //append question and answer choices
-    quiz.innerHTML = ''
-    var num = questions[current]
-    var h2 = document.createElement("h2");
-    h2.textContent = num.question;
-    quiz.appendChild(h2);
-
-    var ol = document.createElement("ol");
-   quiz.appendChild(ol);
-
-    for (i=0; i<num.choices.length; i++) {
-        var li = document.createElement("li");
-        li.textContent = num.choices[i];
-        li.setAttribute("data-index", i);
-        ol.appendChild(li);
-        li.addEventListener('click', function(event){
-            compare(this.getAttribute('data-index'))
-        })
-    } 
+        endQuiz();
     }
-   
+    else{
+        //remove content section of start page
+        document.getElementById('content').style = "display:none";
+
+        var quiz = document.getElementById('quiz');
+        //append question and answer choices to quiz section
+        quiz.innerHTML = ''
+        var num = questions[current]
+        var h4 = document.createElement("h4");
+        h4.textContent = num.question;
+        quiz.appendChild(h4);
+
+        var ul = document.createElement("ul");
+        quiz.appendChild(ul);
+
+        for (i=0; i<num.choices.length; i++) {
+            var li = document.createElement("li");
+            li.textContent = num.choices[i];
+            li.setAttribute("data-index", i);
+            ul.appendChild(li);
+            li.addEventListener('click', function(event){
+                compare(this.getAttribute('data-index'))
+            })
+        }
+        
+        //if last answer was correct, show "Correct!"
+        //if last answer was incorrect, show "Wrong!"
+    } 
 }
 
 //Function to determine if answer is correct or incorrect
 function compare(index) {
-    //if answer chosen==q.answer, show() next q with "Correct!"
-   if(index == questions[current].answer){
-        userScore += 1
-   }else{
-       //if answer chosen!==q.answer, show() next q with "Wrong!"
-    userScore -= 1; 
-   }
-  current ++;
-  console.log(userScore)
-  show()
-   
+    //if answer chosen is correct, score +1
+    if(index == questions[current].answer){
+        userScore += 1;
+    }
+    //if answer chosen is incorrect, score -1
+    else{
+        userScore -= 1; 
+    }
+    current ++;
+    show();
 }
 
 //Function to start timer
@@ -118,7 +126,7 @@ function setTime(event) {
         timer.textContent = "Time: " + secondsLeft;
 
         if(secondsLeft === 0) {
-        endQuiz();
+            endQuiz();
         }
     }, 1000);
 }
